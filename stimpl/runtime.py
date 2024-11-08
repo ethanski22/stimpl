5fee6e79e855d85d39d5fdf7ee37cf9739ab3211
Ethan Skowronski <ethanski22@icloud.com>
2024-11-08 15:36:58 -0500

    Started to work on stimpl
    Saving to switch devices

--- stimpl/runtime.py
+++ stimpl/runtime.py
@@ -24,7 +24,9 @@ class State(object):
 
     def get_value(self, variable_name) -> Any:
         """ TODO: Implement. """
-        return None
+        variable_value = self.value
+        variable_type = self.type
+        return State(variable_name, variable_value, variable_type, self)
 
     def __repr__(self) -> str:
         return f"{self.variable_name}: {self.value}, " + repr(self.next_state)
